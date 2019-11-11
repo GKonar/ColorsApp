bcdc282c4f2b643d58c72dd25fbfe063b3476e1b
Grzegorz Konarski <gk@pubfront.com>
2019-11-11 20:53:45 +0100

    Add withStyles to PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 
 import MiniPalette from './MiniPalette';
 
-export default class PaletteList extends Component {
+const styles = {
+  root: {
+    backgroundColor: "blue"
+  }
+}
+
+class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
 
     return (
-      <div>
-      <MiniPalette/>
-        <h1>React Colors</h1>
-        {
-          palettes.map(palette => (
-            <MiniPalette {...palette}/>
-          ))
-        }
+      <div className={this.props.classes.root}>
+        <MiniPalette/>
+          <h1>React Colors</h1>
+          {
+            palettes.map(palette => (
+              <MiniPalette {...palette}/>
+            ))
+          }
       </div>
     )
   }
 }
+
+export default withStyles(styles)(PaletteList);
