79f4f116f5abfcfba96ce8b786332b121758ad7d
Grzegorz Konarski <gk@pubfront.com>
2019-11-28 13:46:35 +0100

    Remove unnecessary imports

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -10,7 +10,6 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import PaletteMetaFrom from './PaletteMetaForm';
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 
 const drawerWidth = 400;
 
