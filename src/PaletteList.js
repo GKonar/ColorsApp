d3b4129e87b1339f5c795c1e5d05cbaefabbd7ae
Grzegorz Konarski <gk@pubfront.com>
2019-11-28 21:36:39 +0100

    Add delete to MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -13,7 +13,7 @@ class PaletteList extends Component {
   }
 
   render() {
-    const { palettes, classes } = this.props;
+    const { palettes, classes, deletePalette } = this.props;
 
     return (
       <div className={classes.root}>
@@ -28,7 +28,9 @@ class PaletteList extends Component {
                 <MiniPalette 
                 {...palette} 
                 handleClick={() => this.goToPalette(palette.id)}
-                key={palette.id}/>
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}/>
               ))
             }
           </div> 
