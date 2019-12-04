4f3ea2fe1e6b17a9139d8ce39c96784f5eb0618d
Grzegorz Konarski <gk@pubfront.com>
2019-12-04 20:55:19 +0100

    Install react-transition-group, add fade out transition to mini palettes when delete

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import MiniPalette from './MiniPalette';
 
@@ -22,18 +23,20 @@ class PaletteList extends Component {
             <h1 className={classes.title}>React Colors</h1>
             <Link to="/palette/new">Create Palette</Link>
           </nav>
-          <div className={classes.palettes}>
+          <TransitionGroup className={classes.palettes}>
             {
               palettes.map((palette) => (
-                <MiniPalette 
-                {...palette} 
-                handleClick={() => this.goToPalette(palette.id)}
-                handleDelete={deletePalette}
-                key={palette.id}
-                id={palette.id}/>
+                <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                  <MiniPalette 
+                  {...palette} 
+                  handleClick={() => this.goToPalette(palette.id)}
+                  handleDelete={deletePalette}
+                  key={palette.id}
+                  id={palette.id}/>
+                </CSSTransition>
               ))
             }
-          </div> 
+          </TransitionGroup>
         </div>
       </div>
     )
