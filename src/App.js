c092dfcce4f04c410c49ccceb0b997af4db912ad
Grzegorz Konarski <gk@pubfront.com>
2019-12-16 22:49:43 +0100

    Create separate Page component instead of using a div

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PaletteList from './PaletteList';
 import Palette from './Palette';
 import SingleColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
+import Page from './Page';
 
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
@@ -59,48 +60,48 @@ class App extends Component {
               exact 
               path="/palette/new" 
               render={(routeProps) => (
-                <div className='page'>
+                <Page>
                   <NewPaletteForm 
                     savePalette={this.savePalette} 
                     palettes={this.state.palettes} 
                     {...routeProps}
                   />
-                </div>
+                </Page>
               )}
             />
               <Route 
                 exact path="/palette/:paletteId/:colorId" 
                 render={routeProps => ( 
-                  <div className='page'>  
+                  <Page>  
                     <SingleColorPalette 
                       colorId={routeProps.match.params.colorId}
                       palette={generatePalette(
                         this.findPalette(routeProps.match.params.paletteId))}
                     />
-                  </div>
+                  </Page>
                 )}
               />
               <Route 
                 exact path="/" 
                 render={(routeProps) => 
                   (
-                    <div className='page'>  
+                    <Page> 
                       <PaletteList 
                         palettes={this.state.palettes}
                         deletePalette={this.deletePalette} 
                         {...routeProps} />
-                    </div>
+                    </Page>
                   )}
               />
               <Route 
                 exact path="/palette/:id" 
                 render={routeProps => ( 
-                    <div className='page'>   
+                    <Page>   
                       <Palette 
                         palette={generatePalette(
                           this.findPalette(routeProps.match.params.id))}
                       />
-                    </div>
+                    </Page>
                   )
                 }
               />
